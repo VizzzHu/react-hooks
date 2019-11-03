f39d5394090ab637313a63aca1a2bb21e00db55a
Xianlin <woshihu@hotmail.com>
2019-11-02 21:49:40 -0700

    use useState for reactHooksExample component

--- my-app/src/ReactHooksExample.jsx
+++ my-app/src/ReactHooksExample.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from './Row';
 
 const ReactHooksExample = (props) => {
+  const [name, setName] = useState('Mary');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
   return (
     <div className="column">
       <p>React Hooks Example </p>
       <Row label="Name">
-        <input />
+        <input 
+          value={name}
+          onChange={handleNameChange}
+        />
       </Row>
     </div>
   );
 };
 
-export default ReactHooksExample
+export default ReactHooksExample;
   
