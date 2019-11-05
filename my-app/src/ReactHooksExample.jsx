6a621c93436dee7cd5184abacfc48d41a1de3a70
Xianlin <xhu@splunk.com>
2019-11-05 10:12:02 -0800

    add more custom hooks

--- my-app/src/ReactHooksExample.jsx
+++ my-app/src/ReactHooksExample.jsx
@@ -2,6 +2,23 @@ import React, { useState, useContext, useEffect } from 'react';
 import Row from './Row';
 import { ThemeContext } from './ThemeContext';
 
+const useFormInput = (input) => {
+  const [value, setValue] = useState(input);
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleOnChange,
+  };
+};
+
+const useDocumentTitle = (title) => {
+  useEffect(() => {
+    document.title = title; 
+  });
+};
+
 const useWindowWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
 
@@ -17,42 +34,23 @@ const useWindowWidth = () => {
   });
 
   return width;
-}
+};
 
 const ReactHooksExample = (props) => {
-  const [name, setName] = useState('Mary');
-  const [surname, setSurname] = useState('Smith');
+  const name = useFormInput('Mary');
+  const surname = useFormInput('Smith');
   const theme = useContext(ThemeContext);
   const width = useWindowWidth();
-
-  useEffect(() => {
-    document.title = 'New:' + name;
-  })
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  }
-
-  const handleSurnameChange = (e) => {
-    setSurname(e.target.value);
-  }
-
-  
+  useDocumentTitle('New:' + name.value);
 
   return (
     <div className="column" style={{ 'backgroundColor': theme.backgroundColor, 'color': theme.fontColor }}>
       <p>React Hooks Example </p>
       <Row label="Name">
-        <input 
-          value={name}
-          onChange={handleNameChange}
-        />
+        <input {...name} />
       </Row>
       <Row label="Surname">
-        <input
-          value={surname}
-          onChange={handleSurnameChange}
-        />
+        <input {...surname}/>
       </Row>
       <Row label="Width">
           <div>{width}</div>
